84cf289337baf15c52fa1c0ef99a31628b8f495a
Rajeswari1234960 <rajemedagam222@gmail.com>
2025-02-02 09:01:24 +0530

    commiting code1 and emp

---
+++ emp.java
@@ -0,0 +1,29 @@
+class Employee
+
+{
+    //class variables//
+    int empid=101;
+    //class methods//
+     public void display()
+    {
+        System.out.println("101");
+    }
+    String empname;
+    public void String()
+    {
+        System.out.println("raje");
+    }
+    int empdptno;
+     public void display2()
+     {
+        System.out.println("23");
+     }
+     int empjob;
+     public void String2()
+     {
+        System.out.println("engineer");
+     }
+
+}
+
+   
